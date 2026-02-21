052eae473ee84de57466e37bde1ccaffde069f82
Muhammad Sefat <muhammadsefat55@gmail.com>
2026-02-21 15:47:38 +0600

    added prisma

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { prisma } from "./lib/prisma";
 
 const app = express();
 
@@ -12,5 +13,18 @@ app.get("/", (req, res) => {
 
 // Future: import routes from modules
 // app.use("/auth", authRoutes);
+// example
+app.get("/secial", async (req, res) => {
+  const secialty = await prisma.specialty.create({
+    data: {
+      title: "Cardiology",
+    },
+  });
+  res.status(201).json({
+    success: true,
+    message: "Specialty created successfully",
+    data: secialty,
+  });
+});
 
 export default app;
